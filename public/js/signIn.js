602a30c36a28977e4b8fc1c6e6ae9fba8e9db314
sammidunn <sammidunn@hotmail.com>
2021-07-26 17:43:36 +0000

    redirect after sign in functionality

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,7 +1,16 @@
-console.log("signIn.js running");
-
 const signInBtn = document.querySelector(".button");
 signInBtn.addEventListener("click", () => {
     console.log("sign in click");
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+        console.log("login success", user);
+        window.location = "writeNote.html"
+    })
+    .catch(error => {
+        console.log("login failed", error);
+    });
+
 });
 
